3d217ff3a2fee2c86a4c9bac0acc3a6feca4b685
kimjiwon-0821 <wldnjsqkek29@naver.com>
2023-02-14 10:05:01 +0900

    20230214

--- src/main/java/com/iu/s1/member/MemberDTO.java
+++ src/main/java/com/iu/s1/member/MemberDTO.java
@@ -1,11 +1,16 @@
 package com.iu.s1.member;
 
+import java.util.List;
+
 public class MemberDTO {
+	
 	private String id;
 	private String pw;
 	private String name;
 	private String phone;
 	private String email;
+	private List<RoleDTO> roleDTOs;
+	
 	public String getId() {
 		return id;
 	}
@@ -35,6 +40,12 @@ public class MemberDTO {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public List<RoleDTO> getRoleDTOs() {
+		return roleDTOs;
+	}
+	public void setRoleDTOs(List<RoleDTO> roleDTOs) {
+		this.roleDTOs = roleDTOs;
 	}
 	
 	
