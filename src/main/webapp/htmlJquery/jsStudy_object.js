21bc3ff984a58b274c0ef2119fae68d00ef57b57
kimjiwon-0821 <wldnjsqkek29@naver.com>
2023-03-03 11:20:08 +0900

    0303

---
+++ src/main/webapp/htmlJquery/jsStudy_object.js
@@ -0,0 +1,52 @@
+let car={
+    brand:'AUDI',
+    price:3000,
+    info:function(){
+        console.log(this.brand,this.price);
+    }
+}
+
+//class선언
+class Student{
+    #n='';
+    set n(n){
+        this.#n=n;
+    }
+    get  n(){
+        return this.#n;
+    }
+
+    #kor;
+    set kor(kor){
+        this.#kor=kor;
+    }
+    get  kor(){
+        return this.#kor;
+    }
+
+    info(){
+        console.log(this.#n,this.#kor);
+    }
+}
+
+const btn1 = document.getElementById("btn1");
+btn1.addEventListener("click",function(){
+    // console.log(car.brand);
+    // car.info();
+    // car.color = 'red';
+    // for(let v in car){
+    //     console.log(v);
+    // }
+
+    let s = new Student();
+    s.n='iu';
+    s.kor=100;
+    s.eng=100;
+    s.info();
+    console.log(s.eng);
+    console.log(s.n);
+    console.log(s.kor);
+
+    let s1 = new Student();
+})
+
